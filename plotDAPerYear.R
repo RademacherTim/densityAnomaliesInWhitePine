718402d429a2f06a5cf79de6ffaf74661133de05
TTRademacher <rademacher.tim@gmail.com>
2020-06-30 16:35:32 -0700

    Updated figures.

--- plotDAPerYear.R
+++ plotDAPerYear.R
@@ -36,36 +36,35 @@ png (file = 'fig/numberOfAnomaliesPerYearBH.png', width = PNGwidth)
 
 # Plot the frequency of microdensity anomalies per year ring
 #----------------------------------------------------------------------------------------
-par (mar = c (5, 5, 1, 5))
-plot (x = summaryData [['Year']], y = summaryData [['perBH']], col = 'white', 
-      typ = 'l', las = 1, xlab = 'Year', ylim = c (0, 105), xlim = c (1993, 2018),
-      ylab = 'Percentage of annual rings with density anomaly (%)')
-lines (x = summaryData [['Year']], y = summaryData [['perBH']], col = colours [1],
-       lwd = 4)
-points (x = summaryData [['Year']], y = summaryData [['perBH']], col = colours [1], 
-        pch = 21, bg = 'white', lwd = 4)
-
-# Add a legend 
-#----------------------------------------------------------------------------------------
-legend (x = 1994, y = 100, box.lty = 0, pch = 21, col = colours [1], 
-        legend = 'breast height', lwd = 4, lty = 1,
-        bg = 'transparent', pt.bg = 'white', pt.lwd = 2)
-legend (x = 1992.5, y = 100, box.lty = 0, col = colours [1], 
-        legend = '', lwd = 3, lty = 2,
-        bg = 'transparent')
-text (x = 1993.3, y = 103, pos = 1, labels = 'n')
-text (x = 1994.5, y = 103, pos = 1, labels = '%')
-
-# Add line for the sample size at BH
-#----------------------------------------------------------------------------------------
-par (new = TRUE)
-plot (x = summaryData [['Year']], y = summaryData [['nBH']], col = colours [1], lty = 2, 
+par (mar = c (5, 9, 1, 6))
+plot (x = summaryData [['Year']], y = summaryData [['nBH']], col = '#666666', lty = 2, 
       axes = FALSE, typ = 'l', xlab = '', ylab = '', lwd = 3, ylim = c (0, 41))
 
 # Add second y-axis
 #----------------------------------------------------------------------------------------
-axis (side = 4, las = 1)
-mtext (side = 4, line = 3, text = 'Sample size (n)')
+axis (side = 4, las = 1, cex.axis = 2)
+mtext (side = 4, line = 4, text = 'Sample size (n)', cex = 2)
+
+# Add line for frequency
+#----------------------------------------------------------------------------------------
+par (new = TRUE)
+plot (x = summaryData [['Year']], y = summaryData [['perBH']], col = 'white', 
+      typ = 'l', las = 1, xlab = '', ylim = c (0, 105), xlim = c (1993, 2018),
+      ylab = '', axes = FALSE)
+axis (side = 1, cex.axis = 2, at = seq (1990, 2020, by = 5))
+axis (side = 2, cex.axis = 2, las = 1)
+mtext (side = 1, line = 4, cex = 2, text = 'Year')
+mtext (side = 2, line = 4.5, cex = 2, text = 'Percentage of annual rings \n with density anomaly (%)')
+lines (x = summaryData [['Year']], y = summaryData [['perBH']], col = colours [1],
+       lwd = 4)
+points (x = summaryData [['Year']], y = summaryData [['perBH']], col = colours [1], 
+        pch = 21, bg = 'white', lwd = 4, cex = 2)
+
+# Add a legend 
+#----------------------------------------------------------------------------------------
+legend (x = 1992.5, y = 100, box.lty = 0, pch = c (21, 30), col = c (colours [1], '#666666'), 
+        legend = c ('%','n'), lwd = c (4, 3), lty = c (1, 2), cex = 2,
+        bg = 'transparent', pt.bg = 'white', pt.lwd = 4)
 
 # Close ploting device
 #----------------------------------------------------------------------------------------
@@ -77,39 +76,75 @@ png (file = 'fig/numberOfAnomaliesPerYearNearBranch.png', width = PNGwidth)
 
 # Plot the frequency of microdensity anomalies per year ring
 #----------------------------------------------------------------------------------------
-par (mar = c (5, 5, 1, 5))
-plot (x = summaryData [['Year']], y = summaryData [['perBH']], col = 'white', 
-      typ = 'l', las = 1, xlab = 'Year', ylim = c (0, 105), xlim = c (1993, 2018),
-      ylab = 'Percentage of annual rings with density anomaly (%)')
-
-# Add frequency in cores near branches
-#----------------------------------------------------------------------------------------
-lines (x = summaryData [['Year']], y = summaryData [['perBranch']], col = colours [2],
-       lwd = 4)
-points (x = summaryData [['Year']], y = summaryData [['perBranch']], col = colours [2], 
-        lwd = 4, pch = 21, bg = 'white')
-
-# Add a legend 
-#----------------------------------------------------------------------------------------
-legend (x = 1994, y = 100, box.lty = 0, pch = 21, col = colours [2], 
-        legend = 'near-branch', lwd = 4, lty = 1,
-        bg = 'transparent', pt.bg = 'white', pt.lwd = 2)
-legend (x = 1992.5, y = 100, box.lty = 0, col = colours [2], 
-        legend = '', lwd = 2, lty = 3,
-        bg = 'transparent')
-text (x = 1993.3, y = 103, pos = 1, labels = 'n')
-text (x = 1994.5, y = 103, pos = 1, labels = '%')
-
-# Add line for the sample size near branch
-#----------------------------------------------------------------------------------------
-par (new = TRUE)
-plot (x = summaryData [['Year']], y = summaryData [['nBranch']], col = colours [2], lty = 2, 
+par (mar = c (5, 9, 1, 6))
+plot (x = summaryData [['Year']], y = summaryData [['nBranch']], col = '#666666', lty = 2, 
       axes = FALSE, typ = 'l', xlab = '', ylab = '', lwd = 3, ylim = c (0, 41))
 
 # Add second y-axis
 #----------------------------------------------------------------------------------------
-axis (side = 4, las = 1)
-mtext (side = 4, line = 3, text = 'Sample size (n)')
+axis (side = 4, las = 1, cex.axis = 2)
+mtext (side = 4, line = 4, text = 'Sample size (n)', cex = 2)
+
+# Add line for frequency
+#----------------------------------------------------------------------------------------
+par (new = TRUE)
+plot (x = summaryData [['Year']], y = summaryData [['perBranch']], col = 'white', 
+      typ = 'l', las = 1, xlab = '', ylim = c (0, 105), xlim = c (1993, 2018),
+      ylab = '', axes = FALSE)
+axis (side = 1, cex.axis = 2, at = seq (1990, 2020, by = 5))
+axis (side = 2, cex.axis = 2, las = 1)
+mtext (side = 1, line = 4, cex = 2, text = 'Year')
+mtext (side = 2, line = 4.5, cex = 2, text = 'Percentage of annual rings \n with density anomaly (%)')
+lines (x = summaryData [['Year']], y = summaryData [['perBranch']], col = colours [2],
+       lwd = 4)
+points (x = summaryData [['Year']], y = summaryData [['perBranch']], col = colours [2], 
+        pch = 21, bg = 'white', lwd = 4, cex = 2)
+
+# Add a legend 
+#----------------------------------------------------------------------------------------
+legend (x = 1992.5, y = 100, box.lty = 0, pch = c (21, 30), col = c (colours [2], '#666666'), 
+        legend = c ('%','n'), lwd = c (4, 3), lty = c (1, 2), cex = 2,
+        bg = 'transparent', pt.bg = 'white', pt.lwd = 4)
+
+# Close ploting device
+#----------------------------------------------------------------------------------------
+dev.off ()
+
+# Open file for ploting
+#----------------------------------------------------------------------------------------
+png (file = 'fig/numberOfAnomaliesPerYearNearTop.png', width = PNGwidth)
+
+# Plot the frequency of microdensity anomalies per year ring
+#----------------------------------------------------------------------------------------
+par (mar = c (5, 9, 1, 6))
+plot (x = summaryData [['Year']], y = summaryData [['nTop']], col = '#666666', lty = 2, 
+      axes = FALSE, typ = 'l', xlab = '', ylab = '', lwd = 3, ylim = c (0, 41))
+
+# Add second y-axis
+#----------------------------------------------------------------------------------------
+axis (side = 4, las = 1, cex.axis = 2)
+mtext (side = 4, line = 4, text = 'Sample size (n)', cex = 2)
+
+# Add line for frequency
+#----------------------------------------------------------------------------------------
+par (new = TRUE)
+plot (x = summaryData [['Year']], y = summaryData [['perTop']], col = 'white', 
+      typ = 'l', las = 1, xlab = '', ylim = c (0, 105), xlim = c (1993, 2018),
+      ylab = '', axes = FALSE)
+axis (side = 1, cex.axis = 2, at = seq (1990, 2020, by = 5))
+axis (side = 2, cex.axis = 2, las = 1)
+mtext (side = 1, line = 4, cex = 2, text = 'Year')
+mtext (side = 2, line = 4.5, cex = 2, text = 'Percentage of annual rings \n with density anomaly (%)')
+lines (x = summaryData [['Year']], y = summaryData [['perTop']], col = colours [3],
+       lwd = 4)
+points (x = summaryData [['Year']], y = summaryData [['perTop']], col = colours [3], 
+        pch = 21, bg = 'white', lwd = 4, cex = 2)
+
+# Add a legend 
+#----------------------------------------------------------------------------------------
+legend (x = 1992.5, y = 100, box.lty = 0, pch = c (21, 30), col = c (colours [3], '#666666'), 
+        legend = c ('%','n'), lwd = c (4, 3), lty = c (1, 2), cex = 2,
+        bg = 'transparent', pt.bg = 'white', pt.lwd = 4)
 
 # Close ploting device
 #----------------------------------------------------------------------------------------
