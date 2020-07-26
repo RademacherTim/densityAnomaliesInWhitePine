a1afa031f7fb629e1237865bd19c57013c40494d
TTRademacher <rademacher.tim@gmail.com>
2020-07-25 20:24:27 -0400

    Change to clean up plots

--- plotRingWidthVsDA.R
+++ plotRingWidthVsDA.R
@@ -7,7 +7,7 @@
 source ('readGrowthData.R')
 library ('tidyverse')
 
-# Wrangle data to get frequency over ring width for breat height, near-branch and top-of-tree
+# Wrangle data to get frequency over ring width for breat height
 #----------------------------------------------------------------------------------------
 tmp1 <- data %>% filter (!is.na (RingWidthBH_1) & !is.na (DABH_1)) %>% 
   select (RingWidthBH_1, DABH_1) %>% rename (RingWidth = RingWidthBH_1, DABH = DABH_1)
@@ -21,10 +21,15 @@ rhoBHwithout <- density (tmp [['RingWidth']] [tmp [['DABH']] == 0])
 # Plot graph of density distribution by ring width at breast height
 #----------------------------------------------------------------------------------------
 par (mar = c (5, 5, 1, 1))
-plot (rhoBH, las = 1, ylim = c (0, 0.25), xlab = 'Ring width (mm)', lwd = 3,
+plot (rhoBH, las = 1, ylim = c (0, 0.25), xlab = 'Ring width (mm)', lwd = 3, axes = FALSE, 
       ylab = 'Density distribution of density anomalies', main = '', col = '#666666')
-lines (rhoBHwith, col = colours [1], lwd = 2, lty = 1)
-lines (rhoBHwithout, col = colours [1], lwd = 2, lty = 2)
+axis (side = 1)
+axis (side = 2, las = 1)
+lines (rhoBHwith,    col = colours [1], lwd = 3, lty = 1)
+lines (rhoBHwithout, col = colours [1], lwd = 3, lty = 2)
+legend (x = 7, y = 0.25, col = c (rep (colours [1], 2), '#666666'), lty = c (1, 2, 1), 
+        legend = c ('with density anomaly','without density anomaly','all rings'),
+        lwd = 3, box.lty = 0)
 
 # Plot graph of ring width by density anomaly at breast height
 #----------------------------------------------------------------------------------------
@@ -45,7 +50,7 @@ points (x = jitter (tmp  [['DABH']] [tmp [['DABH']] == 2], amount = 0.2),
 boxplot (tmp [['RingWidth']] [tmp [['DABH']] == 2], at = 2, add = TRUE, 
          col = 'transparent', axes = FALSE)
 
-# Wrangle data to get frequency over ring width for breat height, near-branch and top-of-tree
+# Wrangle data to get frequency over ring width for near-branch
 #----------------------------------------------------------------------------------------
 tmp1 <- data %>% filter (!is.na (RingWidthNearBranch_1) & !is.na (DABranch_1)) %>% 
   select (RingWidthNearBranch_1, DABranch_1) %>% 
@@ -54,16 +59,21 @@ tmp2 <- data %>% filter (!is.na (RingWidthNearBranch_2) & !is.na (DABranch_2)) %
   select (RingWidthNearBranch_2, DABranch_2) %>% 
   rename (RingWidth = RingWidthNearBranch_2, DABH = DABranch_2)
 tmp <-  rbind (tmp1, tmp2)
-rhoBH <- density (tmp [['RingWidth']])
-rhoBHwith <- density (tmp [['RingWidth']] [tmp [['DABH']] >= 1])
-rhoBHwithout <- density (tmp [['RingWidth']] [tmp [['DABH']] == 0])
+rhoBranch <- density (tmp [['RingWidth']])
+rhoBranchwith <- density (tmp [['RingWidth']] [tmp [['DABH']] >= 1])
+rhoBranchwithout <- density (tmp [['RingWidth']] [tmp [['DABH']] == 0])
 
 # Plot graph of density distribution by ring width near a branch
 #----------------------------------------------------------------------------------------
-plot (rhoBH, las = 1, ylim = c (0, 0.35), xlab = 'Ring width (mm)', lwd = 3,
+plot (rhoBranch, las = 1, ylim = c (0, 0.35), xlab = 'Ring width (mm)', lwd = 3, axes = FALSE,
       ylab = 'Density distribution of density anomalies', main = '', col = '#666666')
-lines (rhoBHwith,    col = colours [2], lwd = 2, lty = 1)
-lines (rhoBHwithout, col = colours [2], lwd = 2, lty = 2)
+axis (1)
+axis (2, las = 1)
+lines (rhoBranchwith,    col = colours [2], lwd = 3, lty = 1)
+lines (rhoBranchwithout, col = colours [2], lwd = 3, lty = 2)
+legend (x = 7, y = 0.35, col = c (rep (colours [2], 2), '#666666'), lty = c (1, 2, 1), 
+        legend = c ('with density anomaly','without density anomaly','all rings'),
+        lwd = 3, box.lty = 0)
 
 # Plot graph of ring width by density anomaly near a branch
 #----------------------------------------------------------------------------------------
@@ -84,7 +94,7 @@ points (x = jitter (tmp  [['DABH']] [tmp [['DABH']] == 2], amount = 0.2),
 boxplot (tmp [['RingWidth']] [tmp [['DABH']] == 2], at = 2, add = TRUE, 
          col = 'transparent', axes = FALSE)
 
-# Wrangle data to get frequency over ring width for breat height, near-2010 and top-of-tree
+# Wrangle data to get frequency over ring width for top-of-tree
 #----------------------------------------------------------------------------------------
 tmp1 <- data %>% filter (!is.na (RingWidth2010_1) & !is.na (DA2010_1)) %>% 
   select (RingWidth2010_1, DA2010_1) %>% 
@@ -93,16 +103,21 @@ tmp2 <- data %>% filter (!is.na (RingWidth2010_2) & !is.na (DA2010_2)) %>%
   select (RingWidth2010_2, DA2010_2) %>% 
   rename (RingWidth = RingWidth2010_2, DABH = DA2010_2)
 tmp <-  rbind (tmp1, tmp2)
-rhoBH <- density (tmp [['RingWidth']])
-rhoBHwith <- density (tmp [['RingWidth']] [tmp [['DABH']] >= 1])
-rhoBHwithout <- density (tmp [['RingWidth']] [tmp [['DABH']] == 0])
+rho2010 <- density (tmp [['RingWidth']])
+rho2010with <- density (tmp [['RingWidth']] [tmp [['DABH']] >= 1])
+rho2010without <- density (tmp [['RingWidth']] [tmp [['DABH']] == 0])
 
 # Plot graph of density distribution by ring width near a 2010
 #----------------------------------------------------------------------------------------
-plot (rhoBH, las = 1, ylim = c (0, 0.35), xlab = 'Ring width (mm)', lwd = 3,
+plot (rho2010, las = 1, ylim = c (0, 0.35), xlab = 'Ring width (mm)', lwd = 3, axes = FALSE,
       ylab = 'Density distribution of density anomalies', main = '', col = '#666666')
-lines (rhoBHwith,    col = colours [3], lwd = 2, lty = 1)
+axis (1)
+axis (2, las = 1)
+lines (rho2010with,  col = colours [3], lwd = 2, lty = 1)
 lines (rhoBHwithout, col = colours [3], lwd = 2, lty = 2)
+legend (x = 4.5, y = 0.35, col = c (rep (colours [3], 2), '#666666'), lty = c (1, 2, 1), 
+        legend = c ('with density anomaly','without density anomaly','all rings'),
+        lwd = 3, box.lty = 0)
 
 # Plot graph of ring width by density anomaly near a 2010
 #----------------------------------------------------------------------------------------
